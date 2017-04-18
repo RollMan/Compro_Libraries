8a1f78ee75b1e01add7a33a489bce2410e3d749a
RollMan <qwpmb554@gmail.com>
2017-04-18 13:50:56 +0900

    Add split.cpp

--- split.cpp
+++ split.cpp
@@ -1,11 +1,29 @@
-std::vector<std::string> split(std::string str,std::string sep){
-    char* cstr=const_cast<char*>(str.c_str());
-    char* current;
-    std::vector<std::string> arr;
-    current=strtok(cstr,sep.c_str());
-    while(current!=NULL){
-        arr.push_back(current);
-        current=strtok(NULL,sep.c_str());
-    }
-    return arr;
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(std::string str, std::string sep){
+  std::vector<std::string> res;
+  size_t begin = 0;
+  while(1){
+    size_t pos = str.find(sep, begin);
+    if(pos == std::string::npos) break;
+    res.push_back( str.substr(begin, pos-begin) );
+    begin = pos + sep.size();
+  }
+  res.push_back( str.substr(begin, str.length() - begin) );
+  return res;
 }
+
+#ifdef  EBUG
+#include <iostream>
+using namespace std;
+
+int main(void){
+  string str = "abc def ghi jkl";
+  vector<string> v = split(str, "bc");
+  for(int i = 0; i < v.size(); i++){
+    cout << v[i] << endl;
+  }
+  return 0;
+}
+#endif
